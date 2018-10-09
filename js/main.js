63b32f2aedcd3c203edea5f45d6c66943566f654
Alex Kaikatsishvili <alexander.kaykatsishvili@gmail.com>
2018-10-09 16:04:06 +0300

    A lot of new stuff

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 (function($) {
 
-$('.plus-btn').click(function(){
-  $('body').toggleClass('menu-open');
-})
+    $(window).on("load", function() {
+        $(".preloader").addClass("complete");
+        $("h1.name").addClass("animate-reveal");
+        $("ul.headlines").addClass("animate-reveal");
+        $("p.info").addClass("animate-reveal");
+        $("a.animated-arrow").addClass("animate-reveal");
+    });
+
+    $('.plus-btn').click(function() {
+        $('body').toggleClass('menu-open');
+    })
 
 })(jQuery);
